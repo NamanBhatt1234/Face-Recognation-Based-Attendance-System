3bb4abeba239f335747e2c6d7e80d4251a4c689e
Naman Bhatt <65487375+NamanBhatt1234@users.noreply.github.com>
2021-12-17 19:26:46 +0530

    Update student.py

--- student.py
+++ student.py
@@ -418,13 +418,13 @@ class student_window:
                 con=mysql.connector.connect(host="localhost",user="root",password="1234",database="student_data")
                 #The MySQLCursor class instantiates objects that can execute operations such as SQL statements. Cursor objects interact with the MySQL server using a MySQLConnection object.
                 database_access=con.cursor()
-                database_access.execute("select *from `students`")
+                database_access.execute("select *from students")
                 res=database_access.fetchall()
                 id=0
                 #check here
                 for x in res:
                     id+=1
-                database_access.execute("update `students` set `Dep`=%s,`course`=%s,`Year`=%s,`Semester`=%s,`Name`=%s,`Division`=%s,`Roll`=%s,`Gender`=%s,`Dob`=%s,`Email`=%s,`Phone`=%s,`PhotoSample`=%s where `student_id`=%s",
+                database_access.execute("update students set Dep=%s,course=%s,Year=%s,Semester=%s,Name=%s,Division=%s,Roll=%s,Gender=%s,Dob=%s,Email=%s,Phone=%s,PhotoSample=%s where student_id=%s",
                                                (
                                                 self.var_dep.get(),
                                                 self.var_course.get(),
